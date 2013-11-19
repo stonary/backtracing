b2766a3bf61bc0bba5fe76617f090275c31493c3
stonary <shihu1991@gmail.com>
2013-11-19 17:31:12 -0500

    finished part 2

--- A3.Sequential-Assertional.rkt
+++ A3.Sequential-Assertional.rkt
@@ -13,19 +13,38 @@
 (define (assert true/false) (unless true/false (fail)))
 
 #| One of the numbers 200, 100, 25, 10, or 5 (in that order). |#
-(define (coin) '<TODO>)
+(define (coin) (-< 200 100 25 10 5))
 
 #| A coin ≤ `b`. |#
 (define (coin-bounded b)
-  (define c (coin))
-  '<TODO>)
+  (block
+   (define c (coin))
+   (assert (<= c b))
+   c))
 
 #| A non-increasing list of coins adding up to `n`, using only coins ≤ `b`. |#
 (define (change-bounded n b)
-  (-< (block '<TODO>)
-      (block '<TODO>)))
+  (-< (block 
+       (define c (coin-bounded b))
+       (assert (< c n))
+       (cons c (change-bounded (- n c) c)))
+      (block 
+       (define c (coin-bounded b))
+       (assert (= c n))
+       (list c))))
 
 (define (change n) (change-bounded n 200))
 
 (module+ test
+  (displayln "testing assert:")
+  (assert #t)
+  #;(assert #f)
+  (displayln "testing coin:")
+  (coin) (?) (?) (?) (?) (?) 
+  (displayln "testing bounded coin:")
+  (coin-bounded 100) (?) (?) (?) (?)
+  
+  (displayln "testing change-bounded: 200 with <= 100 changes")
+  (change-bounded 200 110) (?) (?) (?) (?) (?) (?) (?) (?) (?) 
+  
   (equal? 450 (length (list-all (change 325)))))
